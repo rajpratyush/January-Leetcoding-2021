66aca41b84e741f90f35089927dc2c07d3b74caf
Pratyush Raj <pratyushraj2fastandfurious@gmail.com>
2021-01-17 09:43:26 +0530

    Create day16.cpp

---
+++ day16.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+      priority_queue<int,vector<int>,greater<int>> pq;
+      int n = nums.size();
+       for(int i=0;i<n;i++)
+       {
+           pq.push(nums[i]);
+           
+           if(pq.size() > k)
+               pq.pop();
+       }
+        int res = pq.top();
+        
+        return res;
+    }
+};
